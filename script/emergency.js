6d55b2b0dd0bfc245c99ed6691e407892e83850b
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-06-29 03:13:27 +0300

    fixed the bug just removed the +1 :)

--- script/emergency.js
+++ script/emergency.js
@@ -1,4 +1,8 @@
 "use strict";
+/////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////// tap color //////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+let path = window.location.pathname;
 let path2 = window.location.pathname;
 
 if (path2 == "/html/emergency.html") {
@@ -6,88 +10,9 @@ if (path2 == "/html/emergency.html") {
 
   covid.setAttribute("style", "color:#00EAD3");
 }
-
-/////////////////////////////////////////////////////////
-let userImgName = document.getElementById("userName");
-let userPic = document.getElementById("userPic");
-userPic.style.display = "none";
-let logOut = document.getElementById("logOut");
-let indexOfLogger;
-
-if (!localStorage.logInStatus) {
-  updateVisualsLogout();
-} else {
-  let currInd = Number(localStorage.index);
-  console.log(currInd);
-  try {
-    updateVisualsLogIn(JSON.parse(localStorage.click)[currInd].name);
-  } catch {
-    updateVisualsLogout();
-  }
-}
-function updateVisualsLogIn(name) {
-  // just show and hide stuff based on if the user signs up
-  if (name) {
-    console.log(name);
-  } else {
-    updateVisualsLogout();
-    return;
-  }
-  name = name
-    .split(" ")
-    .map((item) => item.toUpperCase())
-    .map((item) => {
-      return item[0];
-    })
-    .join("");
-  userImgName.textContent = name;
-  logOut.style.display = "initial";
-  userImgName.style.display = "initial";
-  userPic.style.display = "none";
-} ////////////////////////////////////////
-function updateVisualsLogout() {
-  // just show and hide stuff based on if the user logs out
-  logOut.style.display = "none";
-  userImgName.style.display = "none";
-  userPic.style.display = "initial";
-  localStorage.setItem("logInStatus", false);
-} ////////////////////////////////////////
-
-function logout() {
-  // when you click log out
-  updateVisualsLogout();
-} ////////////////////////////////////////
-
-function checkUser() {
-  // check on login
-  let userPhone = document.getElementById("userPhone").value;
-  let userName = document.getElementById("userNmae").value;
-  let userEmail = document.getElementById("userImael").value;
-
-  let numofUsers = JSON.parse(localStorage.click).length;
-
-  for (let i = 0; i < numofUsers; i++) {
-    let { number, name, email } = JSON.parse(localStorage.click)[i];
-    if (
-      number == userPhone &&
-      name.toLowerCase() == userName.toLowerCase() &&
-      userEmail.toLowerCase() == email.toLowerCase()
-    ) {
-      console.log("login succesfull !");
-      updateVisualsLogIn(name);
-      INPUT.reset();
-      localStorage.setItem("logInStatus", true);
-      localStorage.setItem("index", i);
-      break;
-    } else {
-      INPUT.reset();
-      alert("User does not exist, please check again.");
-      localStorage.setItem("logInStatus", false);
-    }
-  }
-} ////////////////////////////////////////
-////////////////////////////////////////////////////////
-
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// main function ///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 let mainContainer = document.getElementById("mainContainer");
 
 function Emergency(name, instructions) {
@@ -146,13 +71,10 @@ function checkChoice(e) {
       console.log('5555555555555555555555555555555');
       return;
     }
-   
       if (instructionsArray[i].includes(name)) {
         // console.log(i);
         instructions = instructionsArray[i][1].split("/");
       }
-    
-    
   }
   let emergency = new Emergency(name, instructions);
   localStorage.clear();
@@ -165,12 +87,9 @@ function change_page(obj) {
   localStorage.setItem("obj", JSON.stringify(obj));
   window.location.href = "../html/instructions.html";
 }
-
-////////////////////////header ///
-
-
-
-///// foooter image
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// footer image ////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 let tweets=['tweet1.jpg','tweet2.jpg','tweet3.jpg','tweet4.png','tweet5.png','tweet6.png','tweet7.png','tweet8.png','tweet9.png',
 'tweet10.png','tweet11.png','tweet12.png','tweet13.png','tweet14.png','tweet15.png','tweet16.png','tweet17.png','tweet18.png']
 
@@ -192,6 +111,4 @@ function printImage(){
    }
  
 }
-
-
 window.setInterval(printImage,3000);
